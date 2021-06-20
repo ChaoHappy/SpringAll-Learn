933710ce74ff00d67351955b0a55e35ed5af77e6
zhanglichao <871761665@qq.com>
2021-06-20 21:00:17 +0800

    SpringCloud-Learn 服务间通信

--- springcloud-parent/springcloud-05ribbon9997/src/main/java/com/chp/controller/UsersController.java
+++ springcloud-parent/springcloud-05ribbon9997/src/main/java/com/chp/controller/UsersController.java
@@ -3,6 +3,7 @@ package com.chp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,12 @@ import java.util.List;
 public class UsersController {
     @Autowired
     private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/user/findAll")
     public String listAll(){
@@ -28,6 +35,23 @@ public class UsersController {
         String uri = products.get(0).getUri().toString();
         RestTemplate restTemplate = new RestTemplate();
         String str = restTemplate.getForObject(uri+"/product/findAll",String.class);
+        return str;
+    }
+
+    @GetMapping("/user/findAll/loadBalancer")
+    public String listAllLoadBalancer(){
+        ServiceInstance products = loadBalancerClient.choose("productsclient9998");
+        System.out.println("服务主机:"+products.getHost());
+        System.out.println("服务端口:"+products.getPort());
+        System.out.println("服务地址:"+products.getUri());
+        RestTemplate restTemplate = new RestTemplate();
+        String str = restTemplate.getForObject(products.getUri()+"/product/findAll",String.class);
+        return str;
+    }
+
+    @GetMapping("/user/findAll/zhujie/loadBalanced")
+    public String listAllZhujieLoadBalanced(){
+        String str = restTemplate.getForObject("http://productsclient9998/product/findAll",String.class);
         return str;
     }
 }
