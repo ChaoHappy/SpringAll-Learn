cf91144efd96e11b6388c5555f822d7defd06448
ChaoHappy <871761665@qq.com>
2021-08-07 20:23:45 +0800

    Sentinel学习代码

---
+++ springcloud-alibaba-parent/springcloud-alibaba-sentinel8997/src/main/java/com/chp/Sentinel8997Application.java
@@ -0,0 +1,11 @@
+package com.chp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sentinel8997Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Sentinel8997Application.class,args);
+    }
+}
